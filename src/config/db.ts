8ac9df8d96a9aa1c259741a496910f05efbc932b
unknown <javier17utn@gmail.com>
2025-09-08 21:08:07 -0300

    nueva conexion a bd

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,13 +1,14 @@
-import {Sequelize} from 'sequelize-typescript'
+import { Sequelize } from 'sequelize-typescript'
 import dotenv from 'dotenv'
 dotenv.config()
 
-export const db = new Sequelize(process.env.DATABASE_URL,{
-    models:[__dirname + '/../models/**/*'],
-    logging:false,
-    dialectOptions:{
-        ssl:{
-            require:false
-        }
+export const db = new Sequelize(process.env.DATABASE_URL, {
+  models: [__dirname + '/../models/**/*'],
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
     }
+  }
 })
